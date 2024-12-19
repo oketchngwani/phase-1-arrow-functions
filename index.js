9396b2eaed872ca558d0229481a7d0c2fecf6091
Oketch <oketchngwani@gmail.com>
2024-12-20 04:15:33 +0900

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (a, b) => a / b;
+const square = x => x ** 2;
+const add = (a, b) => a + b;
+console.log(divide(2000, 100));
+console.log(square(2));        
+console.log(add(3, 4)); 
